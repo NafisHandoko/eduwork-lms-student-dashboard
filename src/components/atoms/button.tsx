22708e537ac1bfd2fd345e2200113f9922385d5b
Nafis Handoko <naffisioner@gmail.com>
2023-08-31 15:36:27 +0700

    refactor(ui): created action-panel and button component

---
+++ src/components/atoms/button.tsx
@@ -0,0 +1,5 @@
+export default function Button({text}: any) {
+    return (
+        <button className='bg-primary-main rounded-full px-5 py-2 text-white whitespace-nowrap'>{text}</button>
+    )
+}
