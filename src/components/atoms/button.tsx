ca6423779b9d83c12f333e6dbaaea1a1d3935373
Nafis Handoko <naffisioner@gmail.com>
2023-09-03 19:20:18 +0700

    style(button): removed 'bg-primary-main' style/class on button className

--- src/components/atoms/button.tsx
+++ src/components/atoms/button.tsx
@@ -10,6 +10,6 @@ export default function Button({ text, type }: any) {
     }
 
     return (
-        <button className={`bg-primary-main rounded-full px-5 py-2 text-white whitespace-nowrap ${_bg} ${_cr}`}>{text}</button>
+        <button className={`rounded-full px-5 py-2 text-white whitespace-nowrap ${_bg} ${_cr}`}>{text}</button>
     )
 }
