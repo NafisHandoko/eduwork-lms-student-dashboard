f8f70bf9c6fc73546b6c0250b708398c50888620
Nafis Handoko <naffisioner@gmail.com>
2023-09-03 10:20:21 +0700

    style(button): changed button component styling configuration

--- src/components/atoms/button.tsx
+++ src/components/atoms/button.tsx
@@ -1,5 +1,15 @@
-export default function Button({text}: any) {
+export default function Button({ text, type }: any) {
+    let _bg = 'bg-primary-main'
+    let _cr = 'text-white'
+
+    if (type === 'primary') {
+        _bg = 'bg-primary-main'
+    }
+    if (type === 'success') {
+        _bg = 'bg-flowkit-green'
+    }
+
     return (
-        <button className='bg-primary-main rounded-full px-5 py-2 text-white whitespace-nowrap'>{text}</button>
+        <button className={`bg-primary-main rounded-full px-5 py-2 text-white whitespace-nowrap ${_bg} ${_cr}`}>{text}</button>
     )
 }
