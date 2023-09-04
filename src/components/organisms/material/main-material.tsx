81d69b78492c588d5a0b37f91d445a4333975f47
Nafis Handoko <naffisioner@gmail.com>
2023-09-04 21:37:16 +0700

    refactor(ui): added a table to main-material

--- src/components/organisms/material/main-material.tsx
+++ src/components/organisms/material/main-material.tsx
@@ -1,4 +1,4 @@
-import { HiChevronUpDown } from "react-icons/hi2";
+import { HiChevronRight, HiChevronUpDown, HiOutlineClock, HiOutlineEye, HiOutlineLockClosed, HiXMark } from "react-icons/hi2";
 
 export default function MainMaterial() {
     return (
@@ -13,7 +13,7 @@ export default function MainMaterial() {
                     Download slide PowerPoint (.PPTX) <a href="#" className="font-bold underline">DISINI</a>
                 </p>
             </div>
-            <table className="table mt-7">
+            <table className="table table-auto mt-7">
                 <thead className="table-header-group text-text-paragraph">
                     <tr className="table-row bg-[#F8F8F8] border-y border-neutral-40">
                         <th className="table-cell px-7 py-2">
@@ -36,14 +36,53 @@ export default function MainMaterial() {
                     <tr className="table-row border-y border-neutral-40">
                         <td className="table-cell px-7 py-2">Buatlah Ringkasan Fundamental Marketing</td>
                         <td className="table-cell px-7 py-2">48 jam</td>
-                        <td className="table-cell px-7 py-2">locked</td>
-                        <td className="table-cell px-7 py-2">locked</td>
+                        <td className="table-cell px-7 py-2">
+                            <div className="flex flex-row items-center justify-center gap-2 text-[#838383]">
+                                <HiOutlineLockClosed />
+                                <span>locked</span>
+                            </div>
+                        </td>
+                        <td className="table-cell px-7 py-2">
+                            <div className="flex flex-row items-center justify-center gap-1 text-white bg-[#949494] rounded-lg px-3 py-1">
+                                <HiOutlineLockClosed />
+                                <span className="mr-2">locked</span>
+                                <HiChevronRight />
+                            </div>
+                        </td>
                     </tr>
                     <tr className="table-row border-y border-neutral-40">
-                        <td className="table-cell px-7 py-2">Buatlah Ringkasan Fundamental Marketing</td>
+                        <td className="table-cell px-7 py-2">Buatlah Ringkasan Fundamental Marketing coba  judulnya panjang jadi bisa sampe 2 baris </td>
                         <td className="table-cell px-7 py-2">48 jam</td>
-                        <td className="table-cell px-7 py-2">locked</td>
-                        <td className="table-cell px-7 py-2">locked</td>
+                        <td className="table-cell px-7 py-2">
+                            <div className="flex flex-row items-center justify-center gap-1 bg-[#F0FFB2] rounded-full px-3 py-1">
+                                <HiOutlineClock />
+                                <span className="whitespace-nowrap">Sedang diperiksa</span>
+                            </div>
+                        </td>
+                        <td className="table-cell px-7 py-2">
+                            <div className="flex flex-row items-center justify-center gap-1 text-white bg-[#949494] rounded-lg px-3 py-1">
+                                <HiOutlineLockClosed />
+                                <span className="mr-2">locked</span>
+                                <HiChevronRight />
+                            </div>
+                        </td>
+                    </tr>
+                    <tr className="table-row border-y border-neutral-40">
+                        <td className="table-cell px-7 py-2">Buatlah Ringkasan Fundamental Marketing coba  judulnya panjang jadi bisa sampe 2 baris </td>
+                        <td className="table-cell px-7 py-2">48 jam</td>
+                        <td className="table-cell px-7 py-2">
+                            <div className="flex flex-row items-center justify-center gap-1 bg-[#FFE2E2] rounded-full px-3 py-1">
+                                <HiXMark />
+                                <span className="whitespace-nowrap">Belum dikerjakan</span>
+                            </div>
+                        </td>
+                        <td className="table-cell px-7 py-2">
+                            <div className="flex flex-row items-center justify-center gap-1 text-white bg-primary-main rounded-lg px-3 py-1">
+                                <HiOutlineEye />
+                                <span className="mr-2">lihat</span>
+                                <HiChevronRight />
+                            </div>
+                        </td>
                     </tr>
                 </tbody>
             </table>
