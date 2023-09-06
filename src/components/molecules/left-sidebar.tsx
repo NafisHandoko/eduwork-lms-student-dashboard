3fdbc982ab94f160ce2fb79e7be632c0860f627d
Nafis Handoko <naffisioner@gmail.com>
2023-09-07 05:05:56 +0700

    fix(left-sidebar): fixed display issue in left-sidebar when collapsed

--- src/components/molecules/left-sidebar.tsx
+++ src/components/molecules/left-sidebar.tsx
@@ -1,15 +1,25 @@
 import EduworkLogo from '../../assets/eduwork-logo.png'
+import EduworkLogoMini from '../../assets/eduwork-logo-mini.png'
 import SidebarMenu from '../atoms/sidebar-menu'
 
 export default function LeftSidebar({ isExpand }: any) {
     return (
         <div className={`h-full bg-white transition-all py-3 ${isExpand ? 'w-1/6' : 'w-[5%]'}`}>
-            <img src={EduworkLogo} alt="" className='px-6' />
-            <nav className="flex flex-col py-3">
-                <SidebarMenu path="/notifications" icon="globe" text="Notifications" />
-                <SidebarMenu path="/my-class" icon="bullseye" text="My Class" />
-                <SidebarMenu path="/list-class" icon="laptop" text="List Class" />
-            </nav>
+            <div className={`flex items-center justify-center py-2 ${isExpand ? 'px-6' : ''}`}>
+                <img src={isExpand ? EduworkLogo : EduworkLogoMini} alt="" />
+            </div>
+            {isExpand ?
+                <nav className="flex flex-col py-3">
+                    <SidebarMenu path="/notifications" icon="globe" text="Notifications" />
+                    <SidebarMenu path="/my-class" icon="bullseye" text="My Class" />
+                    <SidebarMenu path="/list-class" icon="laptop" text="List Class" />
+                </nav> :
+                <nav className="flex flex-col py-3">
+                    <SidebarMenu path="/notifications" icon="globe" />
+                    <SidebarMenu path="/my-class" icon="bullseye" />
+                    <SidebarMenu path="/list-class" icon="laptop" />
+                </nav>
+            }
         </div>
     )
 }
