671611c1956a4a5dda8d61fb673988c9706b837b
Nafis Handoko <naffisioner@gmail.com>
2023-09-07 18:39:41 +0700

    refactor(forum): separated ForumCard component

--- src/components/organisms/material/forum.tsx
+++ src/components/organisms/material/forum.tsx
@@ -2,10 +2,29 @@ import { HiMagnifyingGlass, HiOutlineChatBubbleLeftEllipsis } from "react-icons/
 import Input from "../../atoms/input";
 import { useState } from 'react'
 
+function ForumCard() {
+    const dummyPhoto = 'https://source.unsplash.com/random/?person'
+
+    return (
+        <div className="flex flex-col p-5 border border-neutral-60 bg-[#FAFAFA] rounded-lg gap-2">
+            <div className="flex flex-row items-center">
+                <img src={dummyPhoto} alt="" className="w-7 h-7 object-cover rounded-full mr-3" />
+                <span>Krisna Dwikurnia</span>
+                <span className="text-neutral-90 text-sm ml-4">・ 1 Jam yang lalu</span>
+            </div>
+            <h3>Silahkan dibaca sebelum Membuat Diskusi Baru atau Menjawab Diskusi</h3>
+            <p className="text-sm text-neutral-90">Membuat Diskusi Baru untuk mempermudah dalam membantu permasalah pada proses pembelajaran Anda..</p>
+            <div className="flex flex-row items-center gap-2 text-neutral-90">
+                <HiOutlineChatBubbleLeftEllipsis />
+                <span className="text-sm">177 Pembahasan</span>
+            </div>
+        </div>
+    )
+}
+
 export default function Forum() {
     const [serachForum, setSearchForum] = useState('')
     const [selectMaterial, setSelectMaterial] = useState('')
-    const dummyPhoto = 'https://source.unsplash.com/random/?person'
 
     return (
         <div className="flex flex-row px-7 items-stretch gap-10">
@@ -38,32 +57,9 @@ export default function Forum() {
                     />
                 </div>
                 <div className="flex flex-col gap-5">
-                    <div className="flex flex-col p-5 border rounded gap-2">
-                        <div className="flex flex-row items-center">
-                            <img src={dummyPhoto} alt="" className="w-7 h-7 object-cover rounded-full mr-3" />
-                            <span>Krisna Dwikurnia</span>
-                            <span className="text-neutral-90 text-sm ml-4">・ 1 Jam yang lalu</span>
-                        </div>
-                        <h3>Silahkan dibaca sebelum Membuat Diskusi Baru atau Menjawab Diskusi</h3>
-                        <p className="text-sm text-neutral-90">Membuat Diskusi Baru untuk mempermudah dalam membantu permasalah pada proses pembelajaran Anda..</p>
-                        <div className="flex flex-row items-center gap-2 text-neutral-90">
-                            <HiOutlineChatBubbleLeftEllipsis />
-                            <span className="text-sm">177 Pembahasan</span>
-                        </div>
-                    </div>
-                    <div className="flex flex-col p-5 border rounded gap-2">
-                        <div className="flex flex-row items-center">
-                            <img src={dummyPhoto} alt="" className="w-7 h-7 object-cover rounded-full mr-3" />
-                            <span>Krisna Dwikurnia</span>
-                            <span className="text-neutral-90 text-sm ml-4">・ 1 Jam yang lalu</span>
-                        </div>
-                        <h3>Silahkan dibaca sebelum Membuat Diskusi Baru atau Menjawab Diskusi</h3>
-                        <p className="text-sm text-neutral-90">Membuat Diskusi Baru untuk mempermudah dalam membantu permasalah pada proses pembelajaran Anda..</p>
-                        <div className="flex flex-row items-center gap-2 text-neutral-90">
-                            <HiOutlineChatBubbleLeftEllipsis />
-                            <span className="text-sm">177 Pembahasan</span>
-                        </div>
-                    </div>
+                    <ForumCard />
+                    <ForumCard />
+                    <ForumCard />
                 </div>
             </div>
         </div>
