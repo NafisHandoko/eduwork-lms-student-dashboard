23f091f3d85a722e2e3fbb15ac4ce16dde054e19
Nafis Handoko <naffisioner@gmail.com>
2023-09-15 23:32:32 +0700

    refactor(ui): added other navigation menu in sidebar

--- src/components/atoms/sidebar-menu.tsx
+++ src/components/atoms/sidebar-menu.tsx
@@ -1,6 +1,9 @@
+import { useSelector } from "react-redux"
 import { NavLink } from "react-router-dom"
 
 export default function SidebarMenu({ path, icon, text }: any) {
+    const isDesktopExpand = useSelector((state: any) => state.sidebar.desktop)
+    
     return (
         <NavLink
             to={path}
@@ -8,8 +11,8 @@ export default function SidebarMenu({ path, icon, text }: any) {
                 `flex flex-row items-center gap-3 px-6 py-3 border-l-4 ${isActive ? 'bg-primary-surface text-primary-main border-primary-main' : 'bg-white text-neutral-70 border-white'}`
             }
         >
-            <i className={`bi bi-${icon}`}></i>
-            <span>{text}</span>
+            {icon}
+            {isDesktopExpand ? <span>{text}</span> : null}
         </NavLink>
     )
 }
--- src/components/molecules/left-sidebar.tsx
+++ src/components/molecules/left-sidebar.tsx
@@ -1,7 +1,7 @@
 import EduworkLogo from '../../assets/eduwork-logo.png'
 import EduworkLogoMini from '../../assets/eduwork-logo-mini.png'
 import SidebarMenu from '../atoms/sidebar-menu'
-import { HiXMark } from 'react-icons/hi2'
+import { HiOutlineBriefcase, HiOutlineCalendarDays, HiOutlineGlobeAsiaAustralia, HiOutlineQuestionMarkCircle, HiOutlineRectangleStack, HiOutlineTrophy, HiOutlineUser, HiOutlineXCircle, HiXMark } from 'react-icons/hi2'
 import { useSelector, useDispatch } from 'react-redux'
 import { toggleSidebar } from '../../store/sidebarSlice'
 
@@ -16,18 +16,16 @@ export default function LeftSidebar() {
                 <div className={`flex items-center justify-center py-2 ${isDesktopExpand ? 'px-6' : 'px-4'}`}>
                     <img src={isDesktopExpand ? EduworkLogo : EduworkLogoMini} alt="" />
                 </div>
-                {isDesktopExpand ?
-                    <nav className="flex flex-col py-3">
-                        <SidebarMenu path="/notifications" icon="globe" text="Notifications" />
-                        <SidebarMenu path="/my-class" icon="bullseye" text="My Class" />
-                        <SidebarMenu path="/list-class" icon="laptop" text="List Class" />
-                    </nav> :
-                    <nav className="flex flex-col py-3">
-                        <SidebarMenu path="/notifications" icon="globe" />
-                        <SidebarMenu path="/my-class" icon="bullseye" />
-                        <SidebarMenu path="/list-class" icon="laptop" />
-                    </nav>
-                }
+                <nav className="flex flex-col py-3">
+                    <SidebarMenu path="/my-class" icon={<HiOutlineRectangleStack />} text="My Class" />
+                    <SidebarMenu path="/events" icon={<HiOutlineCalendarDays />} text="Events" />
+                    <SidebarMenu path="/penyaluran" icon={<HiOutlineBriefcase />} text="Penyaluran" />
+                    <SidebarMenu path="/community" icon={<HiOutlineGlobeAsiaAustralia />} text="Community" />
+                    <SidebarMenu path="/account" icon={<HiOutlineUser />} text="Account" />
+                    <SidebarMenu path="/apresiasi" icon={<HiOutlineTrophy />} text="Apresiasi" />
+                    <SidebarMenu path="/customer-support" icon={<HiOutlineQuestionMarkCircle />} text="Customer Support" />
+                    <SidebarMenu path="/logout" icon={<HiOutlineXCircle />} text="Logout" />
+                </nav>
             </div>
             <div className={`md:hidden bg-white text-black-primary text-center fixed z-20 top-0 right-[100%] transition-all w-1/2 h-screen flex items-center justify-center ${isMobileExpand ? 'right-[50%] md:right-[100%]' : 'right-[100%]'}`}>
                 <button onClick={() => dispatch(toggleSidebar())} className='text-3xl block md:hidden py-5 absolute top-2 right-6'>
