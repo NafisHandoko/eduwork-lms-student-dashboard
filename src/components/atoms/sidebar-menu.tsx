cba9f26ac6fac58b01a5ba0e824d8e42285c284e
Nafis Handoko <naffisioner@gmail.com>
2023-09-17 17:11:08 +0700

    style(left-sidebar): changed some icons and added hover on left-sidebar

--- src/components/atoms/sidebar-menu.tsx
+++ src/components/atoms/sidebar-menu.tsx
@@ -3,12 +3,12 @@ import { NavLink } from "react-router-dom"
 
 export default function SidebarMenu({ path, icon, text }: any) {
     const isDesktopExpand = useSelector((state: any) => state.sidebar.desktop)
-    
+
     return (
         <NavLink
             to={path}
             className={({ isActive, isPending }) =>
-                `flex flex-row items-center gap-3 px-6 py-3 border-l-4 ${isActive ? 'bg-primary-surface text-primary-main border-primary-main' : 'bg-white text-neutral-70 border-white'}`
+                `flex flex-row items-center gap-3 px-6 py-4 border-l-4 ${isActive ? 'bg-primary-surface text-primary-main border-primary-main' : 'bg-white text-neutral-70 border-white hover:bg-primary-surface hover:text-primary-main hover:border-primary-main'}`
             }
         >
             {icon}
--- src/components/molecules/left-sidebar.tsx
+++ src/components/molecules/left-sidebar.tsx
@@ -1,7 +1,7 @@
 import EduworkLogo from '../../assets/eduwork-logo.png'
 import EduworkLogoMini from '../../assets/eduwork-logo-mini.png'
 import SidebarMenu from '../atoms/sidebar-menu'
-import { HiOutlineBriefcase, HiOutlineCalendarDays, HiOutlineGlobeAsiaAustralia, HiOutlineQuestionMarkCircle, HiOutlineRectangleStack, HiOutlineTrophy, HiOutlineUser, HiOutlineXCircle, HiXMark } from 'react-icons/hi2'
+import { HiArrowLeftOnRectangle, HiOutlineBookOpen, HiOutlineBriefcase, HiOutlineCalendarDays, HiOutlineGlobeAsiaAustralia, HiOutlineQuestionMarkCircle, HiOutlineRectangleStack, HiOutlineTrophy, HiOutlineUser, HiOutlineXCircle, HiXMark } from 'react-icons/hi2'
 import { useSelector, useDispatch } from 'react-redux'
 import { toggleSidebar } from '../../store/sidebarSlice'
 
@@ -12,19 +12,19 @@ export default function LeftSidebar() {
 
     return (
         <>
-            <div className={`hidden md:block h-full bg-white transition-all py-3 ${isDesktopExpand ? 'w-1/6' : 'w-[5%]'}`}>
-                <div className={`flex items-center justify-center py-2 ${isDesktopExpand ? 'px-6' : 'px-4'}`}>
+            <div className={`hidden md:block h-full bg-white transition-all ${isDesktopExpand ? 'w-1/6' : 'w-[5%]'}`}>
+                <div className={`flex items-center justify-center h-[70px] ${isDesktopExpand ? 'px-6' : 'px-4'}`}>
                     <img src={isDesktopExpand ? EduworkLogo : EduworkLogoMini} alt="" />
                 </div>
-                <nav className="flex flex-col py-3">
-                    <SidebarMenu path="/my-class" icon={<HiOutlineRectangleStack />} text="My Class" />
-                    <SidebarMenu path="/events" icon={<HiOutlineCalendarDays />} text="Events" />
-                    <SidebarMenu path="/penyaluran" icon={<HiOutlineBriefcase />} text="Penyaluran" />
-                    <SidebarMenu path="/community" icon={<HiOutlineGlobeAsiaAustralia />} text="Community" />
-                    <SidebarMenu path="/account" icon={<HiOutlineUser />} text="Account" />
-                    <SidebarMenu path="/apresiasi" icon={<HiOutlineTrophy />} text="Apresiasi" />
-                    <SidebarMenu path="/customer-support" icon={<HiOutlineQuestionMarkCircle />} text="Customer Support" />
-                    <SidebarMenu path="/logout" icon={<HiOutlineXCircle />} text="Logout" />
+                <nav className="flex flex-col">
+                    <SidebarMenu path="/my-class" icon={<HiOutlineBookOpen className="text-xl" />} text="My Class" />
+                    <SidebarMenu path="/events" icon={<HiOutlineCalendarDays className="text-xl" />} text="Events" />
+                    <SidebarMenu path="/penyaluran" icon={<HiOutlineBriefcase className="text-xl" />} text="Penyaluran" />
+                    <SidebarMenu path="/community" icon={<HiOutlineGlobeAsiaAustralia className="text-xl" />} text="Community" />
+                    <SidebarMenu path="/account" icon={<HiOutlineUser className="text-xl" />} text="Account" />
+                    <SidebarMenu path="/apresiasi" icon={<HiOutlineTrophy className="text-xl" />} text="Apresiasi" />
+                    <SidebarMenu path="/customer-support" icon={<HiOutlineQuestionMarkCircle className="text-xl" />} text="Customer Support" />
+                    <SidebarMenu path="/logout" icon={<HiArrowLeftOnRectangle className="text-xl" />} text="Logout" />
                 </nav>
             </div>
             <div className={`md:hidden bg-white text-black-primary text-center fixed z-20 top-0 right-[100%] transition-all w-1/2 h-screen flex items-center justify-center ${isMobileExpand ? 'right-[50%] md:right-[100%]' : 'right-[100%]'}`}>
