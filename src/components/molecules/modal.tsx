7356b177885111816322552e0358367faaf8e819
Nafis Handoko <naffisioner@gmail.com>
2023-09-17 15:46:28 +0700

    feat(modal): added  confirmation modal when delete class button is clicked

--- src/components/molecules/modal.tsx
+++ src/components/molecules/modal.tsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import { HiExclamationCircle } from "react-icons/hi2";
 
-export default function Modal({ type, title, body, confirmBtnText = 'Confirm', cancelBtnText = 'Cancel', onConfirm = () => { } }: any) {
-    const [isDismissed, setIsDismissed] = useState(false)
-
+export default function Modal({ isDismissed, setIsDismissed, type, title, body, confirmBtnText = 'Confirm', cancelBtnText = 'Cancel', onConfirm = () => { } }: any) {
     return (
         <div className={`fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50 ${isDismissed ? 'hidden' : ''}`}>
             <div className="bg-white rounded-lg flex flex-col w-full md:w-1/3">
@@ -19,7 +16,7 @@ export default function Modal({ type, title, body, confirmBtnText = 'Confirm', c
                 <div className="bg-neutral-20 flex flex-col items-end p-3 rounded-b-lg">
                     <div className="flex flex-row items-center gap-2">
                         <button className="bg-white text-sm text-neutral-90 px-4 py-2 rounded-lg" onClick={() => setIsDismissed(true)}>{cancelBtnText}</button>
-                        <button className={`bg-primary-main text-sm text-white px-4 py-2 rounded-lg ${type == 'danger' ? 'bg-danger-main' : ''}`} onClick={onConfirm}>{confirmBtnText}</button>
+                        <button className={`text-sm text-white px-4 py-2 rounded-lg ${type == 'danger' ? 'bg-danger-main' : 'bg-primary-main'}`} onClick={onConfirm}>{confirmBtnText}</button>
                     </div>
                 </div>
             </div>
