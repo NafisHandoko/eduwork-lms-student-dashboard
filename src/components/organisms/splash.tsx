73da66e8c31c12d14fb70cb2290ce763f7aba683
Nafis Handoko <naffisioner@gmail.com>
2023-09-18 15:27:57 +0700

    build(framer-motion)!: removed framer-motion

--- src/components/organisms/splash.tsx
+++ src/components/organisms/splash.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
 import SplashImg from '../../assets/splash.png'
-import { motion } from "framer-motion"
 
 export default function Splash() {
     const [progress, setProgress] = useState(0)
@@ -22,12 +21,6 @@ export default function Splash() {
                     <span className='text-neutral-90 font-medium text-2xl'>Please Wait</span>
                     <span className='text-neutral-70 text-lg'>We've loaded {progress}% of your data</span>
                     <div className='w-full bg-primary-surface rounded-full h-1.5'>
-                        {/* <motion.div
-                            initial={{ width: 0 }}
-                            animate={{ width: "100%" }}
-                            transition={{ duration: 3 }}
-                            className={`bg-primary-main rounded-full h-full`} style={{ width: `${progress}%` }}
-                        /> */}
                         <div className={`bg-primary-main rounded-full h-full transition-all`} style={{ width: `${progress}%` }}></div>
                     </div>
                 </div>
